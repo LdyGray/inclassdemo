9927e50238ee1cff774049e49f82b884f6fba52e
LdyGray <159605933+LdyGray@users.noreply.github.com>
2024-09-25 16:59:12 -0400

    Adding prompts and two output options, hopefully

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,50 @@
 import streamlit as st
+from openai import OpenAI
+import os
 
-st.title("🎈 My beautiful new app")
+
+st.title("🎈 Homework App")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Please add your prompt here!"
 )
 
-title = st.text_input("Movie title", "Life of Brian")
-st.write("The current movie title is", title)
+title = st.text_input("Prompt", "What is Barcelona?")
+st.write("Barcelona is", title)
+
+
+os.environ["OPENAI_API_KEY"] = st.secrets["OpenAIkey"]
+
+### OpenAI stuff
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": prompt}
+  ],
+  temperature = 1.8,
+  max_tokens=20
+)
+
+### Display
+st.write(
+    "Creative: " + response.choices[0].message.content
+)
+
+
+### OpenAI stuff
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": prompt}
+  ],
+  temperature = 0.2,
+  max_tokens=20
+)
+
+### Display
+st.write(
+    "Predictable: " + response.choices[0].message.content
+)
